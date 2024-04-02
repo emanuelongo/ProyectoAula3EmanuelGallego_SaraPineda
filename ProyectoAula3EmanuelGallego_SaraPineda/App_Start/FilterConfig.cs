5fb35949508daed13932c60247481c0d181bd711
emanuelongo <emanuel1001cano@gmail.com>
2024-04-02 16:44:24 -0500

    Add project files.

---
+++ ProyectoAula3EmanuelGallego_SaraPineda/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoAula3EmanuelGallego_SaraPineda
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
